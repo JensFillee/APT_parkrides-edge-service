eb55e34ac838f175757f9c004d87f27112cbf9ea
JensFillee <r0800028@student.thomasmore.be>
2021-10-12 15:59:56 +0200

    initial commit

---
+++ src/main/java/fact/it/apt_parkridesedgeservice/AptParkridesEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.apt_parkridesedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptParkridesEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptParkridesEdgeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/apt_parkridesedgeservice/AptParkridesEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.apt_parkridesedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptParkridesEdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
